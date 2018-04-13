1bec620201174a04e050d547dafeaed4f174761a
Eberhard Beilharz <eb1@sil.org>
2018-04-13 09:27:47 +0200

    Switch to new csproj format

--- SIL.BuildTasks/Properties/AssemblyInfo.cs
+++ SIL.BuildTasks/Properties/AssemblyInfo.cs
@@ -1,27 +1,5 @@
 ﻿// Copyright (c) 2018 SIL International
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SIL.BuildTasks")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SIL")]
-[assembly: AssemblyProduct("Palaso Library")]
-[assembly: AssemblyCopyright("Copyright © 2018 SIL International")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8a17ca29-630a-4869-8a66-aa05cf5538ba")]
 
 [assembly: InternalsVisibleTo("SIL.BuildTasks.Tests")]
