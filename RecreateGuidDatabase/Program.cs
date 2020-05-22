a9de4c83c3acc5571fa7cd4bcaa5566eb58aed6d
Eberhard Beilharz <eb1@sil.org>
2020-05-22 19:17:40 +0200

    Add RecreateGuidDatabase tool
    This tool re-creates the .guidsForInstaller files by extracting
    the GUIDs from an existing generated .wxs file.

---
+++ RecreateGuidDatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2020 SIL International
+// This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
+
+using System;
+
+namespace RecreateGuidDatabase
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args == null || args.Length != 1)
+			{
+				Console.WriteLine(@"
+This tool re-creates the .guidsForInstaller files by extracting the GUIDs from an existing generated .wxs file.
+
+Usage:
+RecreateGuidDatabase <wxsfile>
+");
+				return;
+			}
+
+			var wxsFile = args[0];
+			new WxsFileProcessor(wxsFile).ProcessWxsFile();
+		}
+
+	}
+}
