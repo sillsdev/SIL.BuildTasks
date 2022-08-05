e6a4465b2b585cfda849316ea5e7842779510830
Eberhard Beilharz <eb1@sil.org>
2022-08-05 09:41:02 +0200

    Replace MarkdownDeep with Markdig

--- SIL.ReleaseTasks/StampChangelogFileWithVersion.cs
+++ SIL.ReleaseTasks/StampChangelogFileWithVersion.cs
@@ -5,9 +5,6 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Runtime.CompilerServices;
-using System.Xml.Linq;
-using System.Xml.XPath;
-using MarkdownDeep;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
