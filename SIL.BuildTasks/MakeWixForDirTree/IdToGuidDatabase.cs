4ee5a4fd13961b6f0032d682512b6afa54aba9ca
tombogle <tom_bogle@sil.org>
2024-10-17 11:06:25 -0400

    Changed SIL International to SIL Global

--- SIL.BuildTasks/MakeWixForDirTree/IdToGuidDatabase.cs
+++ SIL.BuildTasks/MakeWixForDirTree/IdToGuidDatabase.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 SIL International
+// Copyright (c) 2018 SIL Global
 // This software is licensed under the MIT License (http://opensource.org/licenses/MIT)
 /*
  * Class to represent metadata (just GUIDs at the moment) about files.
